99fc414486dd2e75cb2c3608f8dfee946023706f
RobelKasahun <36493926+RobelKasahun@users.noreply.github.com>
2025-02-10 19:22:14 -0800

    Add Bubble Sort C++ implementation

--- cpp_bubble_skeleton.cpp
+++ cpp_bubble_skeleton.cpp
@@ -1,31 +1,128 @@
 //
 // template C++ code for doing bubblesort, W. Bethel, SFSU, 2/2023
 //
-// Your name here
-// Your course number, assignment number here
-// date here
-// 1-sentence description here: bubblesort skeleton code, used hand-crafted array of ints and a fixed problem size of n=10
+// Robel Kasahun Ayelew
+// 2253-CSC-656-01, Coding Project #1
+// 02/10/2025
+// Sort an array a of size n = 10 random integers in ascending order using Bubble Sort algorithm.
 //
+
+// Function Prototypes
+/**
+ * prints an array of size n
+ * @param array is the collection of integers to be printed out
+ * @param n size of the array
+ */
+void print_array(int* array, const int n);
+
+/**
+ * sorts a given array of size n in ascending order
+ * using Bubble Sort algorithm
+ *
+ * @param array is the collection of integer of size n
+ *              that will be sorted in ascending order
+ *
+ * @param n is the size of the array
+ */
+void sort(int* array, const int n);
+
+/**
+ * swaps the values of a and b
+ * @param a is the first integer number to be swapped
+ * @param b is the second intger number to be swapped
+ */
+void swap(int& a, int& b);
+
+/**
+ * inner loop that do swapping and comparing values
+ */
+int innerLoop(const int i, int* array, const int n);
 
 #include <iostream>
 #include <unistd.h>
 
-int
-main (int ac, char *av[])
-{
-   int a[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1}; // replace with your own random 10 ints from www.calculator.net/random-number-generator.html
-   int n = sizeof(a)/sizeof(int);
+int main(int ac, char* av[]) {
+    int a[] = {-2, -1, 0, 64, 24, 88, 41, 22, 65, 31};
+    int n = sizeof(a) / sizeof(int);
 
-   std::cout << " a before sort " << std::endl;
+    std::cout << " a before sort " << std::endl;
 
-   // print out the contents of a before the sort
-   // insert your code here
+    // print out the contents of a before the sort
+    // insert your code here
+    print_array(a, n);
 
-   // do the sort into ascending order. 
-   // insert your code here
+    // do the sort into ascending order. 
+    sort(a, n);
+    // insert your code here
+    std::cout << "\n a after sort " << std::endl;
+    print_array(a, n);
 
 
-   // print out the contents of a after the sort
-   // insert your code here
+    // print out the contents of a after the sort
+    // insert your code here
 }
 // EOF
+
+// Function Definitions
+/**
+ * prints an array of size n
+ * @param array is the collection of integers to be printed out
+ * @param n size of the array
+ */
+void print_array(int* array, const int n) {
+    for (int i = 0; i < n; i++) {
+        std::cout << array[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+/**
+ * swaps the values of a and b
+ * @param a is the first integer number to be swapped
+ * @param b is the second intger number to be swapped
+ */
+void swap(int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+/**
+ * sorts a given array of size n in ascending order
+ * using Bubble Sort algorithm
+ *
+ * @param array is the collection of integer of size n
+ *              that will be sorted in ascending order
+ *
+ * @param n is the size of the array
+ */
+void sort(int* array, const int n) {
+    for (int i = 0; i < n; i++) {
+        int result = innerLoop(i, array, n);
+        if (result == 1) {
+            std::cout << "\nChange were made on loop #" << (i) << std::endl;
+        }
+        else {
+            std::cout << "\nChange were not made on loop #" << (i) << std::endl;
+        }
+        print_array(array, n);
+    }
+}
+
+/**
+ * Swaps and compares values of the array
+ */
+int innerLoop(const int i, int* array, const int n) {
+    int swaps_counter = 0;
+    for (int j = i + 1; j < n; j++) {
+        if (array[i] >= array[j]) {
+            swap(array[i], array[j]);
+            // change made to the array
+            ++swaps_counter;
+        }
+    }
+
+    // returns 1 if changes were made
+    // Otherwise, it returns 0
+    return (swaps_counter > 0) ? 1 : 0;
+}
